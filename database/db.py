0472f00c7ee18fb9a8b18d81ffb072bd9b8baeaa
Sidik90 <sidik90@yandex.ru>
2025-06-08 21:07:54 +0300

    Database

--- database/db.py
+++ database/db.py
@@ -2,59 +2,78 @@ import aiosqlite
 from datetime import datetime, timedelta
 from config.config import DATABASE_FILE
 
+
 async def init_db():
     async with aiosqlite.connect(DATABASE_FILE) as db:
         # Таблица для контактов (заявки на обратный звонок)
-        await db.execute('''CREATE TABLE IF NOT EXISTS contacts (
+        await db.execute(
+            """CREATE TABLE IF NOT EXISTS contacts (
             user_id INTEGER,
             name TEXT,
             contact_info TEXT,
             timestamp TEXT
-        )''')
+        )"""
+        )
         # Таблица для напоминаний
-        await db.execute('''CREATE TABLE IF NOT EXISTS reminders (
+        await db.execute(
+            """CREATE TABLE IF NOT EXISTS reminders (
             user_id INTEGER,
             reminder_text TEXT,
             reminder_time TEXT,
             is_sent INTEGER DEFAULT 0
-        )''')
-        # Таблица для данных о продуктах (БЖУ)
-        await db.execute('''CREATE TABLE IF NOT EXISTS foods (
+        )"""
+        )
+        # Таблица для данных о продуктах (БЖУ) с добавленным полем weight
+        await db.execute(
+            """CREATE TABLE IF NOT EXISTS foods (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
             protein REAL DEFAULT 0.0,
             fat REAL DEFAULT 0.0,
             carbs REAL DEFAULT 0.0,
-            calories REAL DEFAULT 0.0
-        )''')
+            calories REAL DEFAULT 0.0,
+            weight REAL DEFAULT 100.0
+        )"""
+        )
         # Таблица для логов
-        await db.execute('''CREATE TABLE IF NOT EXISTS logs (
+        await db.execute(
+            """CREATE TABLE IF NOT EXISTS logs (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             log_level TEXT NOT NULL,
             message TEXT NOT NULL,
             user_id INTEGER,
             timestamp TEXT NOT NULL
-        )''')
+        )"""
+        )
         await db.commit()
     print("База данных инициализирована.")
+
 
 async def save_contact(user_id, name, contact_info):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     async with aiosqlite.connect(DATABASE_FILE) as db:
-        await db.execute("INSERT INTO contacts (user_id, name, contact_info, timestamp) VALUES (?, ?, ?, ?)",
-                         (user_id, name, contact_info, timestamp))
+        await db.execute(
+            "INSERT INTO contacts (user_id, name, contact_info, timestamp) VALUES (?, ?, ?, ?)",
+            (user_id, name, contact_info, timestamp),
+        )
         await db.commit()
+
 
 async def save_reminder(user_id, reminder_text, reminder_time):
     async with aiosqlite.connect(DATABASE_FILE) as db:
-        await db.execute("INSERT INTO reminders (user_id, reminder_text, reminder_time, is_sent) VALUES (?, ?, ?, 0)",
-                         (user_id, reminder_text, reminder_time))
+        await db.execute(
+            "INSERT INTO reminders (user_id, reminder_text, reminder_time, is_sent) VALUES (?, ?, ?, 0)",
+            (user_id, reminder_text, reminder_time),
+        )
         await db.commit()
+
 
 async def get_due_reminders():
     current_time = datetime.now()
     async with aiosqlite.connect(DATABASE_FILE) as db:
-        cursor = await db.execute("SELECT user_id, reminder_text, reminder_time FROM reminders WHERE is_sent = 0")
+        cursor = await db.execute(
+            "SELECT user_id, reminder_text, reminder_time FROM reminders WHERE is_sent = 0"
+        )
         reminders = await cursor.fetchall()
         due_reminders = []
         print(f"Текущее время: {current_time.strftime('%Y-%m-%d %H:%M:%S')}")
@@ -65,88 +84,130 @@ async def get_due_reminders():
                 # Пробуем разные форматы времени
                 reminder_time = None
                 possible_formats = [
-                    "%Y-%m-%d %H:%M",      # 2025-05-25 17:30
-                    "%Y-%m-%d %H:%M:%S",   # 2025-05-25 17:30:00
-                    "%d.%m.%Y %H:%M",      # 25.05.2025 17:30
-                    "%d.%m.%Y %H:%M:%S"    # 25.05.2025 17:30:00
+                    "%Y-%m-%d %H:%M",  # 2025-05-25 17:30
+                    "%Y-%m-%d %H:%M:%S",  # 2025-05-25 17:30:00
+                    "%d.%m.%Y %H:%M",  # 25.05.2025 17:30
+                    "%d.%m.%Y %H:%M:%S",  # 25.05.2025 17:30:00
                 ]
                 for date_format in possible_formats:
                     try:
-                        reminder_time = datetime.strptime(reminder_time_str, date_format)
+                        reminder_time = datetime.strptime(
+                            reminder_time_str, date_format
+                        )
                         break
                     except ValueError:
                         continue
                 if reminder_time and reminder_time <= current_time:
                     due_reminders.append(reminder)
-                    print(f"Напоминание добавлено в очередь на отправку: {reminder_text} на {reminder_time_str}")
+                    print(
+                        f"Напоминание добавлено в очередь на отправку: {reminder_text} на {reminder_time_str}"
+                    )
                 elif reminder_time:
-                    print(f"Напоминание еще не наступило: {reminder_text} на {reminder_time_str}")
+                    print(
+                        f"Напоминание еще не наступило: {reminder_text} на {reminder_time_str}"
+                    )
                 else:
-                    print(f"Не удалось распознать формат времени для напоминания: {reminder_text} на {reminder_time_str}")
+                    print(
+                        f"Не удалось распознать формат времени для напоминания: {reminder_text} на {reminder_time_str}"
+                    )
             except Exception as e:
-                print(f"Ошибка при обработке напоминания {reminder_text} на {reminder_time_str}: {str(e)}")
+                print(
+                    f"Ошибка при обработке напоминания {reminder_text} на {reminder_time_str}: {str(e)}"
+                )
         print(f"Найденные напоминания для отправки: {due_reminders}")
         return due_reminders
 
+
 async def mark_reminder_sent(user_id, reminder_time):
     async with aiosqlite.connect(DATABASE_FILE) as db:
-        await db.execute("UPDATE reminders SET is_sent = 1 WHERE user_id = ? AND reminder_time = ?",
-                         (user_id, reminder_time))
+        await db.execute(
+            "UPDATE reminders SET is_sent = 1 WHERE user_id = ? AND reminder_time = ?",
+            (user_id, reminder_time),
+        )
         await db.commit()
+
 
 async def get_bju_from_local_db(product_name):
     async with aiosqlite.connect(DATABASE_FILE) as db:
-        cursor = await db.execute("SELECT protein, fat, carbs, calories FROM foods WHERE name LIKE ? LIMIT 1",
-                                  (f"%{product_name}%",))
+        cursor = await db.execute(
+            "SELECT protein, fat, carbs, calories, weight FROM foods WHERE name LIKE ? LIMIT 1",
+            (f"%{product_name}%",),
+        )
         result = await cursor.fetchone()
         if result:
-            return {"b": result[0], "j": result[1], "u": result[2], "kcal": result[3]}
+            return {
+                "b": result[0],
+                "j": result[1],
+                "u": result[2],
+                "kcal": result[3],
+                "weight": result[4],
+            }
         return None
+
 
 async def insert_test_foods():
     test_data = [
-        ("яблоко", 0.4, 0.4, 9.8, 47),
-        ("банан", 1.5, 0.2, 21.8, 95),
-        ("курица", 23.1, 1.9, 0, 110)
+        ("яблоко", 0.4, 0.4, 9.8, 47, 100.0),
+        ("банан", 1.5, 0.2, 21.8, 95, 100.0),
+        ("курица", 23.1, 1.9, 0, 110, 100.0),
     ]
     async with aiosqlite.connect(DATABASE_FILE) as db:
         for data in test_data:
-            await db.execute("INSERT OR IGNORE INTO foods (name, protein, fat, carbs, calories) VALUES (?, ?, ?, ?, ?)", data)
+            await db.execute(
+                "INSERT OR IGNORE INTO foods (name, protein, fat, carbs, calories, weight) VALUES (?, ?, ?, ?, ?, ?)",
+                data,
+            )
         await db.commit()
     print("Тестовые данные о продуктах добавлены.")
+
 
 async def save_log_to_db(log_level, message, user_id=None):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     async with aiosqlite.connect(DATABASE_FILE) as db:
-        await db.execute("INSERT INTO logs (log_level, message, user_id, timestamp) VALUES (?, ?, ?, ?)",
-                         (log_level, message, user_id, timestamp))
+        await db.execute(
+            "INSERT INTO logs (log_level, message, user_id, timestamp) VALUES (?, ?, ?, ?)",
+            (log_level, message, user_id, timestamp),
+        )
         await db.commit()
+
 
 async def get_all_logs(limit=50):
     async with aiosqlite.connect(DATABASE_FILE) as db:
-        cursor = await db.execute("SELECT id, log_level, message, user_id, timestamp FROM logs ORDER BY timestamp DESC LIMIT ?", (limit,))
+        cursor = await db.execute(
+            "SELECT id, log_level, message, user_id, timestamp FROM logs ORDER BY timestamp DESC LIMIT ?",
+            (limit,),
+        )
         logs = await cursor.fetchall()
         return logs
 
+
 async def get_all_reminders():
     async with aiosqlite.connect(DATABASE_FILE) as db:
-        cursor = await db.execute("SELECT user_id, reminder_text, reminder_time, is_sent FROM reminders ORDER BY reminder_time")
+        cursor = await db.execute(
+            "SELECT user_id, reminder_text, reminder_time, is_sent FROM reminders ORDER BY reminder_time"
+        )
         reminders = await cursor.fetchall()
         return reminders
 
+
 async def get_all_contacts():
     async with aiosqlite.connect(DATABASE_FILE) as db:
-        cursor = await db.execute("SELECT user_id, name, contact_info, timestamp FROM contacts ORDER BY timestamp DESC")
+        cursor = await db.execute(
+            "SELECT user_id, name, contact_info, timestamp FROM contacts ORDER BY timestamp DESC"
+        )
         contacts = await cursor.fetchall()
         return contacts
+
 
 async def clean_old_reminders(days=7):
     """Удаление старых напоминаний, отправленных более 'days' дней назад."""
     async with aiosqlite.connect(DATABASE_FILE) as db:
-        threshold_time = (datetime.now() - timedelta(days=days)).strftime("%Y-%m-%d %H:%M:%S")
+        threshold_time = (datetime.now() - timedelta(days=days)).strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
         cursor = await db.execute(
             "DELETE FROM reminders WHERE reminder_time < ? AND is_sent = 1",
-            (threshold_time,)
+            (threshold_time,),
         )
         deleted_count = cursor.rowcount
         await db.commit()
--- handlers/reminder.py
+++ handlers/reminder.py
@@ -8,23 +8,28 @@ from database.db import save_reminder
 from utils.logger import logger
 from datetime import datetime
 
-from project.Bots.NutriLifeBot.database.db import get_all_reminders
+from NutriLifeBot.database.db import get_all_reminders
 
 router = Router()
+
 
 # Определение состояний для напоминаний
 class ReminderStates(StatesGroup):
     TEXT = State()
     TIME = State()
 
+
 # Обработчик выбора "Напоминалка"
 @router.callback_query(F.data == "reminder")
 async def start_reminder(callback_query: CallbackQuery, state: FSMContext):
     user_id = callback_query.from_user.id
     logger.info(f"Пользователь {user_id} начал создание напоминания")
-    await callback_query.message.edit_text("Введите текст напоминания:", reply_markup=get_cancel_keyboard())
+    await callback_query.message.edit_text(
+        "Введите текст напоминания:", reply_markup=get_cancel_keyboard()
+    )
     await state.set_state(ReminderStates.TEXT)
     await callback_query.answer()
+
 
 # Обработчик отмены на этапе ввода текста
 @router.callback_query(ReminderStates.TEXT, F.data == "cancel")
@@ -32,8 +37,11 @@ async def cancel_reminder(callback_query: CallbackQuery, state: FSMContext):
     user_id = callback_query.from_user.id
     logger.info(f"Пользователь {user_id} отменил создание напоминания")
     await state.clear()
-    await callback_query.message.edit_text("Создание напоминания отменено.", reply_markup=get_main_menu())
+    await callback_query.message.edit_text(
+        "Создание напоминания отменено.", reply_markup=get_main_menu()
+    )
     await callback_query.answer()
+
 
 # Обработчик ввода текста напоминания
 @router.message(ReminderStates.TEXT)
@@ -42,17 +50,26 @@ async def process_reminder_text(message: Message, state: FSMContext):
     reminder_text = message.text
     logger.info(f"Пользователь {user_id} ввел текст напоминания: {reminder_text}")
     await state.update_data(reminder_text=reminder_text)
-    await message.answer("Введите дату и время напоминания (в формате ДД.ММ.ГГГГ ЧЧ:ММ):", reply_markup=get_cancel_keyboard())
+    await message.answer(
+        "Введите дату и время напоминания (в формате ДД.ММ.ГГГГ ЧЧ:ММ):",
+        reply_markup=get_cancel_keyboard(),
+    )
     await state.set_state(ReminderStates.TIME)
+
 
 # Обработчик отмены на этапе ввода времени
 @router.callback_query(ReminderStates.TIME, F.data == "cancel")
 async def cancel_reminder_time(callback_query: CallbackQuery, state: FSMContext):
     user_id = callback_query.from_user.id
-    logger.info(f"Пользователь {user_id} отменил создание напоминания на этапе ввода времени")
+    logger.info(
+        f"Пользователь {user_id} отменил создание напоминания на этапе ввода времени"
+    )
     await state.clear()
-    await callback_query.message.edit_text("Создание напоминания отменено.", reply_markup=get_main_menu())
+    await callback_query.message.edit_text(
+        "Создание напоминания отменено.", reply_markup=get_main_menu()
+    )
     await callback_query.answer()
+
 
 # Обработчик ввода времени напоминания
 @router.message(ReminderStates.TIME)
@@ -63,16 +80,31 @@ async def process_reminder_time(message: Message, state: FSMContext):
     reminder_text = data.get("reminder_text")
     try:
         # Конвертируем формат ДД.ММ.ГГГГ ЧЧ:ММ в ГГГГ-ММ-ДД ЧЧ:ММ
-        reminder_time = datetime.strptime(reminder_time_input, "%d.%m.%Y %H:%M").strftime("%Y-%m-%d %H:%M")
+        reminder_time = datetime.strptime(
+            reminder_time_input, "%d.%m.%Y %H:%M"
+        ).strftime("%Y-%m-%d %H:%M")
         await save_reminder(user_id, reminder_text, reminder_time)
-        await message.answer(f"Напоминание '{reminder_text}' установлено на {reminder_time_input} ⏰", reply_markup=get_main_menu())
+        await message.answer(
+            f"Напоминание '{reminder_text}' установлено на {reminder_time_input} ⏰",
+            reply_markup=get_main_menu(),
+        )
         logger.info(f"Пользователь {user_id} установил напоминание на {reminder_time}")
     except ValueError:
-        await message.answer("Ошибка: неверный формат даты и времени. Используйте формат ДД.ММ.ГГГГ ЧЧ:ММ (например, 20.10.2023 15:30). Попробуйте снова.", reply_markup=get_cancel_keyboard())
-        logger.warning(f"Пользователь {user_id} ввел неверный формат времени: {reminder_time_input}")
+        await message.answer(
+            "Ошибка: неверный формат даты и времени. Используйте формат ДД.ММ.ГГГГ ЧЧ:ММ (например, 20.10.2023 15:30). Попробуйте снова.",
+            reply_markup=get_cancel_keyboard(),
+        )
+        logger.warning(
+            f"Пользователь {user_id} ввел неверный формат времени: {reminder_time_input}"
+        )
     except Exception as e:
-        await message.answer("Произошла ошибка при сохранении напоминания. Попробуйте снова.", reply_markup=get_cancel_keyboard())
-        logger.error(f"Ошибка при сохранении напоминания для пользователя {user_id}: {str(e)}")
+        await message.answer(
+            "Произошла ошибка при сохранении напоминания. Попробуйте снова.",
+            reply_markup=get_cancel_keyboard(),
+        )
+        logger.error(
+            f"Ошибка при сохранении напоминания для пользователя {user_id}: {str(e)}"
+        )
     finally:
         await state.clear()
 
@@ -97,53 +129,87 @@ async def check_reminders_command(message: Message):
         response += f"   Статус: {status}\n"
         response += "---\n"
     await message.answer(response)
-    logger.info(f"Пользователь {user_id} запросил список напоминаний: найдено {len(user_reminders)} напоминаний")
+    logger.info(
+        f"Пользователь {user_id} запросил список напоминаний: найдено {len(user_reminders)} напоминаний"
+    )
+
 
 @router.message(lambda message: message.text == "/clear_invalid_reminders")
 async def clear_invalid_reminders(message: Message):
     user_id = message.from_user.id
     async with aiosqlite.connect("nutrilife.db") as db:
-        cursor = await db.execute("SELECT reminder_text, reminder_time FROM reminders WHERE user_id = ? AND is_sent = 0", (user_id,))
+        cursor = await db.execute(
+            "SELECT reminder_text, reminder_time FROM reminders WHERE user_id = ? AND is_sent = 0",
+            (user_id,),
+        )
         reminders = await cursor.fetchall()
         invalid_count = 0
         for reminder_text, reminder_time_str in reminders:
             try:
-                if reminder_time_str.count('.') == 2:
+                if reminder_time_str.count(".") == 2:
                     datetime.strptime(reminder_time_str, "%d.%m.%Y %H:%M")
                 else:
                     datetime.strptime(reminder_time_str, "%Y-%m-%d %H:%M")
             except ValueError:
-                await db.execute("DELETE FROM reminders WHERE user_id = ? AND reminder_time = ?", (user_id, reminder_time_str))
+                await db.execute(
+                    "DELETE FROM reminders WHERE user_id = ? AND reminder_time = ?",
+                    (user_id, reminder_time_str),
+                )
                 invalid_count += 1
         await db.commit()
-    await message.answer(f"Удалено {invalid_count} напоминаний с некорректным форматом времени.")
-    logger.info(f"Пользователь {user_id} удалил {invalid_count} некорректных напоминаний")
+    await message.answer(
+        f"Удалено {invalid_count} напоминаний с некорректным форматом времени."
+    )
+    logger.info(
+        f"Пользователь {user_id} удалил {invalid_count} некорректных напоминаний"
+    )
+
 
 @router.message(lambda message: message.text == "/force_send_reminders")
 async def force_send_reminders(message: Message, bot):
     user_id = message.from_user.id
     current_time = datetime.now()
     async with aiosqlite.connect("nutrilife.db") as db:
-        cursor = await db.execute("SELECT user_id, reminder_text, reminder_time FROM reminders WHERE is_sent = 0 AND user_id = ?", (user_id,))
+        cursor = await db.execute(
+            "SELECT user_id, reminder_text, reminder_time FROM reminders WHERE is_sent = 0 AND user_id = ?",
+            (user_id,),
+        )
         reminders = await cursor.fetchall()
         sent_count = 0
         for reminder in reminders:
             uid, reminder_text, reminder_time_str = reminder
             try:
                 reminder_time = None
-                possible_formats = ["%Y-%m-%d %H:%M", "%Y-%m-%d %H:%M:%S", "%d.%m.%Y %H:%M", "%d.%m.%Y %H:%M:%S"]
+                possible_formats = [
+                    "%Y-%m-%d %H:%M",
+                    "%Y-%m-%d %H:%M:%S",
+                    "%d.%m.%Y %H:%M",
+                    "%d.%m.%Y %H:%M:%S",
+                ]
                 for date_format in possible_formats:
                     try:
-                        reminder_time = datetime.strptime(reminder_time_str, date_format)
+                        reminder_time = datetime.strptime(
+                            reminder_time_str, date_format
+                        )
                         break
                     except ValueError:
                         continue
                 if reminder_time and reminder_time <= current_time:
-                    await bot.send_message(chat_id=user_id, text=f"Напоминание (принудительная отправка): {reminder_text} ⏰")
-                    await db.execute("UPDATE reminders SET is_sent = 1 WHERE user_id = ? AND reminder_time = ?", (user_id, reminder_time_str))
+                    await bot.send_message(
+                        chat_id=user_id,
+                        text=f"Напоминание (принудительная отправка): {reminder_text} ⏰",
+                    )
+                    await db.execute(
+                        "UPDATE reminders SET is_sent = 1 WHERE user_id = ? AND reminder_time = ?",
+                        (user_id, reminder_time_str),
+                    )
                     sent_count += 1
             except Exception as e:
-                logger.error(f"Ошибка при принудительной отправке напоминания {reminder_text} на {reminder_time_str}: {str(e)}")
+                logger.error(
+                    f"Ошибка при принудительной отправке напоминания {reminder_text} на {reminder_time_str}: {str(e)}"
+                )
         await db.commit()
     await message.answer(f"Принудительно отправлено {sent_count} напоминаний.")
-    logger.info(f"Пользователь {user_id} принудительно отправил {sent_count} напоминаний")
+    logger.info(
+        f"Пользователь {user_id} принудительно отправил {sent_count} напоминаний"
+    )
